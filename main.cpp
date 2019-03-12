415d10818950205ca0ce7486e31282b0208727af
Bob (ShipilovAV) <bob@example.com>
2019-03-12 16:48:25 +0300

    (code: min)

--- main.cpp
+++ main.cpp
@@ -4,12 +4,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter A and B: ";
+       cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
 cout << "A + B = " << a + b << '\n';
 cout << "A - B = " << a - b << '\n';
 cout << "A * B = " << a * b << '\n';
 cout << "A / B = " << a / b << '\n';
+int min;
+min = a;
+if (min < b) {
+    b = min;
+}
+cout << "min(A,B) = " << min;
     return 0;
 }
